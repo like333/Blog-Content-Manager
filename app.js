7358a85bf69d05e22e0189892bc34fa2c3c9faac
like333 <1046751649@qq.com>
2020-11-03 14:46:50 +0800

    配置管理页面模板

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const home = require('./route/home')
 const admin = require('./route/admin')
+const path  =require('path')
 
+//创建服务器
 const app = express()
 
-// 为理由匹配路径
+//开放静态资源文件
+app.use(express.static(__dirname + '/public'))
+
+// 配置使用的模板引擎
+app.engine('art',require('express-art-template'))
+// 配置模板路径
+app.set('views',path.join(__dirname,'views'))
+// 配置默认模板后缀
+app.set('view engine','art')
+
+
+// 为路由匹配路径
 app.use('/home',home)
 app.use('/admin',admin)
 
+
+
 const server = app.listen(80,()=>{
-    console.log('网站服务器启动成功，请访问localhost')
+    console.log('网站服务器启动成功，请访问localhost',server.address().port)
 })
--- route/admin.js
+++ route/admin.js
@@ -2,7 +2,7 @@
 const express = require('express')
 
 const admin = express.Router()
-admin.get('/',(req,res) => {
-    res.send('欢迎来到博客管理页')
+admin.get('/login',(req,res) => {
+    res.render('admin/login')
 })
 module.exports = admin
