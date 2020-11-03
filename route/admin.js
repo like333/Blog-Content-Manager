7a7ce2d5ff64d410143eee26d25380214176c39a
like333 <1046751649@qq.com>
2020-11-03 15:01:20 +0800

    提取管理页模板公共部分

--- route/admin.js
+++ route/admin.js
@@ -5,4 +5,7 @@ const admin = express.Router()
 admin.get('/login',(req,res) => {
     res.render('admin/login')
 })
+admin.get('/user',(req,res) => {
+    res.render('admin/user')
+})
 module.exports = admin
